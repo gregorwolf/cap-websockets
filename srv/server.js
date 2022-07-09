91c9674ccd2d096d5a784b58dae7a323ebded5db
Gregor Wolf <gregor.wolf@gmail.com>
2022-07-09 22:34:22 +0200

    add job to send CPU usage to WS clients

--- srv/server.js
+++ srv/server.js
@@ -1,29 +1,55 @@
 /* eslint-disable no-console */
 const cds = require("@sap/cds");
 const { WebSocketServer } = require("ws");
+var CronJob = require("cron").CronJob;
+var osu = require("node-os-utils");
+var cpu = osu.cpu;
 
+var job = new CronJob(
+  "*/5 * * * * *",
+  async function () {
+    const cpuUsage = await cpu.usage(100);
+    console.log(`The cpuUsage was ${cpuUsage} %`);
+    sendMessageToConnectedClients({ usage: cpuUsage });
+  },
+  null,
+  false,
+  "Europe/Berlin"
+);
 // WebSocket library
 const wss = new WebSocketServer({ noServer: true });
 
 // react on bootstrapping events...
 cds.on("listening", ({ server }) => {
-	console.log("--> listening");
+  console.log("--> listening");
 
-	server.on("upgrade", (request, socket, head) => {
+  server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+      console.log("WSS HandleUpgrade");
+      wss.emit("connection", ws, request);
+    });
+  });
 
-		wss.handleUpgrade(request, socket, head, ws => {
-			console.log("WSS HandleUpgrade");
-			wss.emit("connection", ws, request);
-		});
+  global.wss = wss;
 
-	});
+  // Use this if the 4th param is default value(false)
+  job.start();
+});
 
-	global.wss = wss;
-
+cds.on("shutdown", () => {
+  console.log("--> shutdown");
+  job.stop();
 });
 
 // handle and override options
 module.exports = (options) => {
-	// delegate to default server.js
-	return cds.server(options);
+  // delegate to default server.js
+  return cds.server(options);
 };
+
+function sendMessageToConnectedClients(messageContent) {
+  // Loop through all connected clients
+  for (const client of global.wss.clients) {
+    client.send(JSON.stringify(messageContent));
+  }
+}
