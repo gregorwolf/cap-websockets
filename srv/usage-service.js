5339b015bb01f8875c1e0cd37f89fd0e9addfaea
Gregor Wolf <gregor.wolf@gmail.com>
2025-03-11 16:50:42 +0100

    switch to @cap-js-community/websocket

--- srv/usage-service.js
+++
@@ -1,9 +0,0 @@
-const cds = require("@sap/cds");
-var osu = require("node-os-utils");
-var cpu = osu.cpu;
-
-module.exports = function () {
-  this.on("READ", "cpu", async (req) => {
-    return { usage: await cpu.usage(100), user: req.user.id };
-  });
-};
