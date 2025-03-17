cb8f8b990e95e074c175bfb6962ba03e67456472
Gregor Wolf <gregor.wolf@gmail.com>
2025-03-17 09:34:52 +0100

    generic Update / Insert handler

--- srv/usage-plugin-service.js
+++ srv/usage-plugin-service.js
@@ -3,6 +3,14 @@ const { INSERT } = require("@sap/cds/lib/ql/cds-ql");
 const LOG = cds.log("usage-plugin-service");
 
 module.exports = (srv) => {
+  srv.on(["UPDATE", "INSERT"], "*", async (req, next) => {
+    LOG.info(`Received data: ${JSON.stringify(req.data)}`);
+    if (req.query?.INSERT) {
+      LOG.info(`Target: ${req.query.INSERT.into.ref[0]}`);
+    }
+    next();
+  });
+
   srv.on("cpu", async (req, next) => {
     LOG.info(`Received cpu usage: ${req.data.usage}`);
     await INSERT.into("UsageData").entries({
